d0075f32865f183a2f675fbf4083b67c0750aee6
Bruno Wladimir <brunowladimir@MacBook-Air-de-Bruno.local>
2024-01-23 08:25:05 -0300

    configuracao menu2

--- src/components/barranavegacao.tsx
+++ src/components/barranavegacao.tsx
@@ -15,7 +15,7 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
-
+import Menu_item from './menuitem';
 const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
@@ -47,24 +47,6 @@ function BarraNavegacao() {
       <AppBar position="relative" >
       <Container maxWidth="xl" className='bg-gray-900'>
         <Toolbar disableGutters>
-          {/* <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography> */}
-
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
             <IconButton
               size="large"
@@ -94,66 +76,8 @@ function BarraNavegacao() {
                 display: { xs: 'block', md: 'none' },
               }}
             >
-{/* 
-<Route path="/" element={<Index/>}>  </Route>
+              <Menu_item/>
 
-<Route path="/sobre" element={<Sobre/>}>  </Route>
-<Route path="/dash" element={<Dash/>}>  </Route>
-<Route path="/pesquisa" element={<Pesquisa/>}>  </Route> */}
-
-              <MenuItem key="/sobre" onClick={handleCloseNavMenu}>
-              <Link to={'/sobre'} > 
-                  <Typography textAlign="center">sobre</Typography>
-                  </Link>
-                </MenuItem>
-
-                <MenuItem key="/index" onClick={handleCloseNavMenu}>
-              <Link to={'/'} > 
-                  <Typography textAlign="center">Index</Typography>
-                  </Link>
-                </MenuItem>
-
-                <MenuItem key="/dash" onClick={handleCloseNavMenu}>
-              <Link to={'/dash'} > 
-                  <Typography textAlign="center">Dash</Typography>
-                  </Link>
-                </MenuItem>
-
-
-                <MenuItem key="/pesquisa" onClick={handleCloseNavMenu}>
-              <Link to={'/pesquisa'} > 
-                  <Typography textAlign="center">Pesquisa</Typography>
-                  </Link>
-                </MenuItem>
-
-
-          {/* <Link to="/sobre">        
-              <Button
-                
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                Sobre
-              </Button>
-          </Link>
-        
-          <Link to="/dash">        
-              <Button
-                
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                Dashboard
-              </Button>
-          </Link> */}
-
-
-
-              {/* {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))} */}
             </Menu>
           </Box>
           <div className='w-1/2 h-auto'> 
@@ -177,47 +101,28 @@ function BarraNavegacao() {
           >
             <img src='Opine.png' ></img>
           </Typography> */}
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-          <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-        
-          </Box>
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <Menu
               id="menu-appbar"
-              anchorEl={anchorElUser}
+              anchorEl={anchorElNav}
               anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: 'bottom',
+                horizontal: 'left',
               }}
               keepMounted
               transformOrigin={{
                 vertical: 'top',
-                horizontal: 'right',
+                horizontal: 'left',
               }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
             >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
+              <Menu_item/>
+
             </Menu>
           </Box>
         </Toolbar>
---
+++ src/components/menuitem.tsx
@@ -0,0 +1,56 @@
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import MenuItem from '@mui/material/MenuItem';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function menuitem() {
+
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+
+    return(
+<>
+<MenuItem key="/sobre" onClick={handleCloseNavMenu}>
+              <Link to={'/sobre'} > 
+                  <Typography textAlign="center">sobre</Typography>
+                  </Link>
+                </MenuItem>
+
+                <MenuItem key="/index" onClick={handleCloseNavMenu}>
+              <Link to={'/'} > 
+                  <Typography textAlign="center">Index</Typography>
+                  </Link>
+                </MenuItem>
+
+                <MenuItem key="/dash" onClick={handleCloseNavMenu}>
+              <Link to={'/dash'} > 
+                  <Typography textAlign="center">Dash</Typography>
+                  </Link>
+                </MenuItem>
+
+
+                <MenuItem key="/pesquisa" onClick={handleCloseNavMenu}>
+              <Link to={'/pesquisa'} > 
+                  <Typography textAlign="center">Pesquisa</Typography>
+                  </Link>
+                </MenuItem>
+                </>
+    )
+}
