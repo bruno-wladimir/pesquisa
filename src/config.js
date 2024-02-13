d889d94ccdf60fc9e52d990ac707c0feb36abf3d
Bruno Wladimir <brunowladimir@Air-de-Bruno.bbrouter>
2024-02-12 21:32:46 -0300

    pagian de entrada user estava bloqueada

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const config = {
 
     //apiUrl: 'https://server-pesquisa.onrender.com',
    //apiUrl: 'http://localhost:3000',
-   apiUrl:"http://ec2-3-89-229-157.compute-1.amazonaws.com:3000",
+   apiUrl:"https://ec2-3-89-229-157.compute-1.amazonaws.com:3000",
 
   };
   
